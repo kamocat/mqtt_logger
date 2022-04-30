35ed2c1963ddd22611d08b2bc355d5dca0f219e1
Marshal Horn <kamocat@gmail.com>
2022-04-30 08:38:51 -0700

    matches topics correctly

--- sqlite.py
+++ sqlite.py
@@ -1,19 +1,31 @@
 import sqlite3
+import types
 con = sqlite3.connect("test.db")
 cur = con.cursor()
 try:
     cur.execute('''create table topics
-            (id int not null primary key, topic varchar(255))''')
+            (topic varchar(255))''')
     cur.execute('''create table num
-            (id int not null primary key, topic int, value real, start timestamp, 
-            end timestamp)''')
+            (topic int, value real, start timestamp, end timestamp)''')
     con.commit()
 except:
     print("tables already created")
 
-def match_topic(t):
-    cur.execute(f"select * from topics where topic=:name",{"name":t})
-    print(cur.fetchone())
+def match_topic(t)
+    # Check if the topic exists
+    cur.execute("select rowid from topics where topic=:name",{"name":t})
+    v = cur.fetchone()
+    try:
+        v = v[0]
+    except:
+        # Insert the new topic
+        cur.execute("insert into topics (topic) values (:n)", {"n":t})
+        con.commit()
+        # Get the index
+        cur.execute("select rowid from topics order by rowid desc")
+        v = cur.fetchone()
+        v = v[0]
+    return v
     
 
 match_topic("test/setpoint")
