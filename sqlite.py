503163632622cce436e080ae35a50514416256ad
Marshal Horn <kamocat@gmail.com>
2022-04-30 10:36:39 -0700

    insert works well

--- sqlite.py
+++ sqlite.py
@@ -1,35 +1,49 @@
 import sqlite3
-import types
-con = sqlite3.connect("test.db")
-cur = con.cursor()
-try:
-    cur.execute('''create table topics
-            (topic varchar(255))''')
-    cur.execute('''create table num
-            (topic int, value real, start timestamp, end timestamp)''')
-    con.commit()
-except:
-    print("tables already created")
+import time
+import math
 
-def match_topic(t)
-    # Check if the topic exists
-    cur.execute("select rowid from topics where topic=:name",{"name":t})
-    v = cur.fetchone()
-    try:
-        v = v[0]
-    except:
-        # Insert the new topic
-        cur.execute("insert into topics (topic) values (:n)", {"n":t})
-        con.commit()
-        # Get the index
-        cur.execute("select rowid from topics order by rowid desc")
-        v = cur.fetchone()
-        v = v[0]
-    return v
-    
+class db:
+    def __init__ (self, path):
+        self.con = sqlite3.connect(path)
+        self.cur = self.con.cursor()
+        try:
+            self.cur.execute('''create table topics
+                (topic varchar(255))''')
+            self.cur.execute('''create table num
+                (topic int, value real, start timestamp, end timestamp)''')
+            self.con.commit()
+        except:
+            pass
 
-match_topic("test/setpoint")
-match_topic("invalid")
+    def insert(self, topic, data):
+        try:
+            data = float(data)
+        except:
+            print(f"Could not convert to number: {data}")
+            return
+        t = time.time()
+        # Check if the value is the same
+        self.cur.execute('''select rowid,value from num 
+        where topic=:n order by rowid desc''', {'n':topic})
+        tmp = self.cur.fetchone()
+        try:
+            prev = {'rowid':tmp[0], 'value':tmp[1], 'end':t}
+            assert(math.isclose(prev["value"],data))
+            self.cur.execute('''update num set end=:end where
+                    rowid=:rowid''',prev)
+        except:
+            # Add the new value
+            self.cur.execute('''insert into num (topic, value, start, end) 
+            values (?,?,?,?)''', (topic, data, t, t))
+        self.con.commit()
 
 
-con.close()
+    def close(self):
+        self.con.close()
+
+
+# Test code
+if __name__ == '__main__':
+    a = db('test.db')
+    a.insert('test/two', '35.3')
+    a.close()
