73996e68e0c772d0df65e0c8594afcf75cce5aef
Marshal Horn <kamocat@gmail.com>
2022-04-30 11:15:53 -0700

    re-introduced topics table

--- sqlite.py
+++ sqlite.py
@@ -14,6 +14,19 @@ class db:
             self.con.commit()
         except:
             pass
+    def match_topic(self, t):
+        # Check if the topic exists
+        self.cur.execute("select rowid from topics where topic=:name",{"name":t})
+        v = self.cur.fetchone()
+        try:
+            v = v[0]
+        except:
+            # Insert the new topic
+            self.cur.execute("insert into topics (topic) values (:n)", {"n":t})
+            self.con.commit()
+            # Get the index
+            v = self.cur.lastrowid
+        return v
 
     def insert(self, topic, data):
         try:
@@ -21,10 +34,11 @@ class db:
         except:
             print(f"Could not convert to number: {data}")
             return
+        n = self.match_topic(topic)
         t = time.time()
         # Check if the value is the same
         self.cur.execute('''select rowid,value from num 
-        where topic=:n order by rowid desc''', {'n':topic})
+        where topic=:n order by rowid desc''', {'n':n})
         tmp = self.cur.fetchone()
         try:
             prev = {'rowid':tmp[0], 'value':tmp[1], 'end':t}
@@ -34,7 +48,7 @@ class db:
         except:
             # Add the new value
             self.cur.execute('''insert into num (topic, value, start, end) 
-            values (?,?,?,?)''', (topic, data, t, t))
+            values (?,?,?,?)''', (n, data, t, t))
         self.con.commit()
 
 
@@ -45,5 +59,6 @@ class db:
 # Test code
 if __name__ == '__main__':
     a = db('test.db')
+    #print(a.match_topic("test/one"))
     a.insert('test/two', '35.3')
     a.close()
