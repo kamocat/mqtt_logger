085c387e71dccf9c307ec25faca721bd46d7b6ce
Marshal Horn <kamocat@gmail.com>
2022-05-21 07:53:22 -0700

    initChart() works

--- graph/helper.js
+++ graph/helper.js
@@ -1,59 +1,61 @@
 let settings = {
-  curve = d3.curveLinear, // method of interpolation between points
-  marginTop = 20, // top margin, in pixels
-  marginRight = 30, // right margin, in pixels
-  marginBottom = 30, // bottom margin, in pixels
-  marginLeft = 40, // left margin, in pixels
-  width = 640, // outer width, in pixels
-  height = 400, // outer height, in pixels
-  yFormat, // a format specifier string for the y-axis
-  yLabel, // a label for the y-axis
-  color = "currentColor", // fill color of area
-  id= "#chart",
+  curve: d3.curveLinear, // method of interpolation between points
+  marginTop: 20, // top margin, in pixels
+  marginRight: 30, // right margin, in pixels
+  marginBottom: 30, // bottom margin, in pixels
+  marginLeft: 40, // left margin, in pixels
+  width: 640, // outer width, in pixels
+  height: 400, // outer height, in pixels
+  color: "currentColor", // fill color of area
+  id: "#chart",
 };
-function InitChart( settings ){
-	let div = d3.select(id);
+function InitChart( conf ){
+	let div = d3.select(settings.id);
     const svg = d3.create("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .attr("viewBox", [0, 0, width, height])
+            .attr("width", conf.width)
+            .attr("height", conf.height)
+            .attr("viewBox", [0, 0, conf.width, conf.height])
             .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+    
+    const xRange = [conf.marginLeft, conf.width - conf.marginRight]; // [left, right]
+    const yRange = [conf.height - conf.marginBottom, conf.marginTop]; // [bottom, top]
+    const domain = [-1,1];
+    const xScale = d3.scaleLinear(domain, xRange);
+    const yScale = d3.scaleLinear(domain, yRange);
+    const xAxis = d3.axisBottom(xScale).ticks(conf.width / 80).tickSizeOuter(0);
+    const yAxis = d3.axisLeft(yScale).ticks(conf.height / 40);
     svg.append("g")
             .attr("class", "yaxis")
-            .attr("transform", `translate(${marginLeft},0)`)
+            .attr("transform", `translate(${conf.marginLeft},0)`)
             .call(yAxis)
-            .call(g => g.select(".domain").remove())
-            .call(g => g.selectAll(".tick line").clone()
-                    .attr("x2", width - marginLeft - marginRight)
-                    .attr("stroke-opacity", 0.1))
-            .call(g => g.append("text")
-                    .attr("x", -marginLeft)
-                    .attr("y", 10)
-                    .attr("fill", "currentColor")
-                    .attr("text-anchor", "start")
-                    .text(yLabel));
     svg.append("g")
             .attr("class", "xaxis")
-            .attr("transform", `translate(0,${height - marginBottom})`)
+            .attr("transform", `translate(0,${conf.height - conf.marginBottom})`)
             .call(xAxis);
     div.node().append(svg.node());
 }
 function updateAxes(alldata, conf){
-  xRange = [conf.marginLeft, conf.width - conf.marginRight], // [left, right]
-  yRange = [conf.height - conf.marginBottom, conf.marginTop], // [bottom, top]
-  // Get min and max of x and y for all plots
-  const X = data.time.map(x=>x*1000);
-  const Y = data.value;
+    xRange = [conf.marginLeft, conf.width - conf.marginRight], // [left, right]
+    yRange = [conf.height - conf.marginBottom, conf.marginTop], // [bottom, top]
+                                                                       // Get min and max of x and y for all plots
+    x = [];
+    y = [];
+    for (d of alldata){
+        x.push(d3.min(alldata[d].time));
+        x.push(d3.max(alldata[d].time));
+        y.push(d3.min(alldata[d].value));
+        y.push(d3.max(alldata[d].value));
+    }
 
-  // Compute default domains.
-  xDomain = d3.extent(X);
-  yDomain = [d3.min(Y), d3.max(Y)];
+    // Compute default domains.
+    xDomain = [d3.min(x)*1000, d3.max(x)*1000];
+    yDomain = [d3.min(y), d3.max(y)];
 
-  // Construct scales and axes.
-  conf.xScale = d3.scaleTime(xDomain, xRange);
-  conf.yScale = d3.scaleLinear(yDomain, yRange);
-  const xAxis = d3.axisBottom(conf.xScale).ticks(width / 80).tickSizeOuter(0);
-  const yAxis = d3.axisLeft(conf.yScale).ticks(height / 40, yFormat);
+    // Construct scales and axes.
+    conf.xScale = d3.scaleTime(xDomain, xRange);
+    conf.yScale = d3.scaleLinear(yDomain, yRange);
+    const xAxis = d3.axisBottom(conf.xScale).ticks(width / 80).tickSizeOuter(0);
+    const yAxis = d3.axisLeft(conf.yScale).ticks(height / 40, yFormat);
 
     svg = d3.select("svg")
     svg = svg.transition().duration(750);
@@ -69,15 +71,18 @@ function make_line(data, settings){
     return line(data.time.length);
 }
 function addPlot(data, settings){
-	svg.append("path")
-                .attr("id", data.i)
-				.attr("fill", "none")
-				.attr("d", make_line(data,settings))
-				.attr("stroke", settings.color)
-				.attr("stroke-width", 1.5);
+    d3.select("svg").append("path")
+            .attr("id", data.i)
+            .attr("fill", "none")
+            .attr("d", make_line(data,settings))
+            .attr("stroke", settings.color)
+            .attr("stroke-width", 1.5);
 }
+
 function updatePlot(data, settings){
-    svg.select(".line").attr("d", make_line(data,settings));
+    svg.select("#"+ data.i)
+            .attr("d", make_line(data,settings))
+            .transition().duration(750);
 }
 function deletePlot(data, settings){
         d3.select("#"+ data.i).remove();
