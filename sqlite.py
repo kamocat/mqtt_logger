e821cac62d1ffe8a1aa4ab8afb9a90679d81da83
Marshal Horn <kamocat@gmail.com>
2022-05-29 10:13:29 -0700

    Need to fix async init

--- sqlite.py
+++ sqlite.py
@@ -1,42 +1,42 @@
-import sqlite3
+import aiosqlite
 import time
 import math
 import datetime
 
 class db:
-    def __init__ (self, path):
-        self.con = sqlite3.connect(path)
-        self.cur = self.con.cursor()
+    async def __init__ (self, path):
+        self.con = await aiosqlite.connect(path)
+        self.cur = await self.con.cursor()
         try:
-            self.cur.execute('''create table topics
+            await self.cur.execute('''create table topics
                 (id integer primary key asc, topic varchar(255))''')
-            self.cur.execute('''create table num
+            await self.cur.execute('''create table num
                 (topic int, value real, start timestamp, end timestamp)''')
-            self.con.commit()
+            await self.con.commit()
         except:
             pass
 
-    def fetch_topic(self, t):
+    async def fetch_topic(self, t):
         # Check if the topic exists
-        self.cur.execute("select id from topics where topic=:name",{"name":t})
-        v = self.cur.fetchone()
+        await self.cur.execute("select id from topics where topic=:name",{"name":t})
+        v = await self.cur.fetchone()
         try:
             v = v[0]
         except:
             v = -1
         return v
 
-    def insert_topic(self, t):
-        n = self.fetch_topic(topic)
+    async def insert_topic(self, t):
+        n = await self.fetch_topic(topic)
         if n<1:
             # Insert the new topic
-            self.cur.execute("insert into topics (topic) values (:n)", {"n":t})
-            self.con.commit()
+            await self.cur.execute("insert into topics (topic) values (:n)", {"n":t})
+            await self.con.commit()
             # Get the index
-            n = self.cur.lastrowid
+            n = await self.cur.lastrowid
         return n
 
-    def insert(self, topic, data, t='now'):
+    async def insert(self, topic, data, t='now'):
         try:
             data = float(data)
         except:
@@ -45,33 +45,33 @@ class db:
         if( t == 'now' ):
             t = time.time()
         # Check if the value is the same
-        n = self.cur.insert_topic(topic)
-        self.cur.execute('''select rowid,value from num 
+        n = await self.cur.insert_topic(topic)
+        await self.cur.execute('''select rowid,value from num 
         where topic=:n order by rowid desc''', {'n':n})
-        tmp = self.cur.fetchone()
+        tmp = await self.cur.fetchone()
         try:
             prev = {'rowid':tmp[0], 'value':tmp[1], 'end':t}
             assert(math.isclose(prev["value"],data))
-            self.cur.execute('''update num set end=:end where
+            await self.cur.execute('''update num set end=:end where
                     rowid=:rowid''',prev)
         except:
             # Add the new value
-            self.cur.execute('''insert into num (topic, value, start, end) 
+            await self.cur.execute('''insert into num (topic, value, start, end) 
             values (?,?,?,?)''', (n, data, t, t))
-        self.con.commit()
+        await self.con.commit()
 
-    def search(self, topic_text):
+    async def search(self, topic_text):
         ret = {}
         topic_text += '%' # append wildcard
         query = 'select id,topic from topics where topic like :n'
-        for row in self.cur.execute(query,{"n":topic_text}):
+        async for row in self.cur.execute(query,{"n":topic_text}):
             ret[row[1]] = row[0]
         return ret
 
-    def fill(self, topic, qty, clear=False):
+    async def fill(self, topic, qty, clear=False):
         if( clear ):
             n = self.insert_topic(topic)
-            self.cur.execute('delete from num where topic=:n',{'n':n})
+            await self.cur.execute('delete from num where topic=:n',{'n':n})
 
         import random as rand
         rand.seed()
@@ -83,18 +83,18 @@ class db:
         for i in range(qty):
             if(rand.random() > 0.8):
                 x += rand.uniform(-0.2,0.5)
-            self.insert(topic, offset+math.cos(x), t+i*300)
+            await self.insert(topic, offset+math.cos(x), t+i*300)
 
-    def fetch(self, topic, end_date=None, days=1):
+    async def fetch(self, topic, end_date=None, days=1):
         timespan = days * 60*60*24
-        n = self.fetch_topic(topic)
+        n = await self.fetch_topic(topic)
         x = []
         y = []
         if(end_date == None):
             # Get the most recent point
-            self.cur.execute('select end from num where topic=:n order by rowid desc limit 1',{'n':n})
-            end_date = self.cur.fetchone()[0]
-        for row in self.cur.execute('select start,value,end from num where topic=:n and start<=:s and end>=:e',{'n':n, 's':end_date, 'e':end_date-timespan}):
+            await self.cur.execute('select end from num where topic=:n order by rowid desc limit 1',{'n':n})
+            end_date = await self.cur.fetchone()[0]
+        async for row in self.cur.execute('select start,value,end from num where topic=:n and start<=:s and end>=:e',{'n':n, 's':end_date, 'e':end_date-timespan}):
             x.append(row[0])
             y.append(row[1])
             if(row[2] != row[0]): #Show the duration of the point
@@ -102,11 +102,11 @@ class db:
                 y.append(row[1])
         return {"time":x, "value":y}
 
-    def latest(self, topic):
-        n = self.fetch_topic(topic)
-        self.cur.execute('select start,end,value from num where topic=:n order by rowid desc limit 1',{'n':n})
-        res = self.cur.fetchone()
+    async def latest(self, topic):
+        n = await self.fetch_topic(topic)
+        await self.cur.execute('select start,end,value from num where topic=:n order by rowid desc limit 1',{'n':n})
+        res = await self.cur.fetchone()
         return {'start':res[0], 'end':res[1], 'value':res[2]}
         
-    def close(self):
-        self.con.close()
+    async def close(self):
+        await self.con.close()
