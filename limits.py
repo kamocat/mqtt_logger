82ba3058a2a7ed703f8828760f14212191d4e533
Marshal <kamocat@gmail.com>
2022-06-01 08:31:31 -0700

    Added hysterisis to prevent repeated notifications

--- limits.py
+++ limits.py
@@ -1,8 +1,9 @@
 import time
 import asyncio
 import mailjet
+import typing
 class Alarm:
-    def __init__(self, topic, notify, timeout=30):
+    def __init__(self, topic:str, notify:list[str], timeout:float=30):
         self.topic = topic
         self.notify = notify
         self.limits = []
@@ -16,13 +17,12 @@ class Alarm:
         if time.monotonic() > self.expire and self.alive:
             self.alive = False
             self.send_alert(f'{self.topic} stopped responding')
-    def limit(self, test, msg):
-        try: test(0)
-        except: print(f'Warning: Test {test} does not accept numeric data')
-        self.limits.append({'test':test, 'msg':msg, 'state':False})
+    def limit(self, limit_pass:float, limit_fail:float, msg:str):
+        self.limits.append({'lpass':limit_pass, 'lfail':limit_fail, 'state':False, 'msg':msg})
     def send_alert(self, msg):
-        mailjet.send(msg, self.notify)
-    def test(self, topic, value):
+        #mailjet.send(msg, self.notify)
+        print(f'Sent email to {self.notify}: {msg}')
+    def test(self, topic:str, value):
         if topic == self.topic:
             self.feed()
             try: value = float(value)
@@ -30,10 +30,14 @@ class Alarm:
                 print(f'Warning: {value} is not numeric')
                 return
             for limit in self.limits:
-                new_state = limit['test'](value)
-                if new_state and not limit['state']:
+                flip = limit['lpass']>limit['lfail']
+                lfail = (limit['lfail'] < value) != flip
+                lpass = (limit['lpass'] > value) != flip
+                if lfail and not limit['state']:
                     self.send_alert(limit['msg'])
-                limit['state'] = new_state
+                    limit['state'] = True
+                elif lpass and limit['state']:
+                    limit['state'] = False
            
 
 async def watchdog(topics):
@@ -47,8 +51,8 @@ marshal = "mhece.api@gmail.com"
 limits = []
 
 test = Alarm(topic='/test', notify=[marshal], timeout=1)
-test.limit(test=(lambda x: x>3), msg='Test value is too high')
-test.limit(test=(lambda x: x<=0), msg='Test value is too low')
+test.limit(limit_pass = 3.5, limit_fail = 4.1, msg='Test value is too high')
+test.limit(limit_pass = 1, limit_fail = 0, msg='Test value is too low')
 
 
 limits.append(test)
