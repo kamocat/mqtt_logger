6cb945c0594b54deacc9207249aedae59e6e43d3
Marshal Horn <kamocat@gmail.com>
2022-05-31 21:54:03 -0700

    Limits and timeout work

--- alert.py
+++ alert.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import limits
+import asyncio
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
@@ -10,10 +11,19 @@ def on_message(client, userdata, msg):
     for alert in limits.limits:
         alert.test(msg.topic, msg.payload)
     
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
 
-client.loop_forever()
+async def main():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("localhost", 1883, 60)
+
+    client.loop_start()
+
+    await limits.watchdog(limits.limits)
+    
+
+asyncio.run(main())
+
