4ba0a9176d9879aa0e657f856ede51ebee9a023c
Marshal Horn <kamocat@gmail.com>
2022-05-31 21:54:03 -0700

    Limits and timeout work

--- limits.py
+++ limits.py
@@ -1,8 +1,20 @@
+import time
+import asyncio
 class Alarm:
-    def __init__(self, topic, notify):
+    def __init__(self, topic, notify, timeout=30):
         self.topic = topic
         self.notify = notify
         self.limits = []
+        self.interval = timeout * 60 #max seconds between messages
+        self.alive = True
+        self.feed()
+    def feed(self):
+        self.expire = time.monotonic() + self.interval
+        self.alive = True
+    def watchdog(self):
+        if time.monotonic() > self.expire and self.alive:
+            self.alive = False
+            self.send_alert(f'{self.topic} stopped responding')
     def limit(self, test, msg):
         try: test(0)
         except: print(f'Warning: Test {test} does not accept numeric data')
@@ -12,6 +24,7 @@ class Alarm:
         #TODO: Send actual email
     def test(self, topic, value):
         if topic == self.topic:
+            self.feed()
             try: value = float(value)
             except: 
                 print(f'Warning: {value} is not numeric')
@@ -23,13 +36,17 @@ class Alarm:
                 limit['state'] = new_state
            
 
-
+async def watchdog(topics):
+    while(1):
+        for topic in topics:
+            topic.watchdog()
+        await asyncio.sleep(5)
 
 marshal = 'kamocat@gmail.com'
 
 limits = []
 
-test = Alarm(topic='/test', notify=[marshal])
+test = Alarm(topic='/test', notify=[marshal], timeout=1)
 test.limit(test=(lambda x: x>3), msg='Test value is too high')
 test.limit(test=(lambda x: x<=0), msg='Test value is too low')
 
